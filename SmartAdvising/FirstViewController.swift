73157c51caa4e9e934f8e0590b3765f6c1c77907
Cameron <cameron.farzaneh@gmail.com>
2019-03-27 20:30:25 -0400

    Initial Commit

---
+++ SmartAdvising/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstViewController.swift
+//  SmartAdvising
+//
+//  Created by Cameron Farzaneh on 3/27/19.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SmartAdvising/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  SmartAdvising
+//
+//  Created by Cameron Farzaneh on 3/27/19.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
